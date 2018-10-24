224aa7b49956c9507dc6b2fd9bffce9924196391
bikilone <milosbirovljevic@gmail.com>
2018-10-24 17:33:30 +0200

    port fixed

--- app.js
+++ app.js
@@ -58,6 +58,6 @@ app.get("/", function (req, res) {
     res.render("home-page");
 });
 
-app.listen("3000", process.env.IP, function () {
+app.listen(process.env.PORT, process.env.IP, function () {
         console.log("server has started")
     });
